ba41a79411303e566bac3474bdc611d8ce36c17e
Hang Yu <allanyu1130@gmail.com>
2023-01-05 15:21:59 -0800

    add asset and information list

--- menubar/AppDelegate.swift
+++ menubar/AppDelegate.swift
@@ -12,28 +12,33 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     var menuBarItem: NSStatusItem!
     var capsLockStatus = false {
         didSet {
-            // update the icon to reflect the caps lock status
-            if capsLockStatus {
-                menuBarItem.button?.image = NSImage(systemSymbolName: "star", accessibilityDescription: "star")
-            } else {
-                menuBarItem.button?.image = NSImage(systemSymbolName: "circle", accessibilityDescription: "circle")
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+//                if self.menuBarItem == nil {
+//                    self.menuBarItem = NSStatusBar.system.statusItem(withLength: 10)
+//                }
+                if self.capsLockStatus {
+                    self.menuBarItem.button?.image = NSImage(named: "caps-lock-on-24")
+                } else {
+                    self.menuBarItem.button?.image = NSImage(named: "caps-lock-off-24")
+//                    NSStatusBar.system.removeStatusItem(self.menuBarItem)
+//                    self.menuBarItem = nil
+                }
             }
         }
     }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-           // create the menu bar item
-           menuBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
-        
+        // create the menu bar item
+        menuBarItem = NSStatusBar.system.statusItem(withLength: 10)
 
-           // set the initial icon based on the caps lock status
-           let event = CGEvent(keyboardEventSource: nil, virtualKey: 0x39, keyDown: true)!
-           capsLockStatus = event.flags.contains(.maskAlphaShift)
+        // set the initial icon based on the caps lock status
+        let event = CGEvent(keyboardEventSource: nil, virtualKey: 0x39, keyDown: true)!
+        capsLockStatus = event.flags.contains(.maskAlphaShift)
         // create a global event monitor to continuously monitor keyboard events
         NSEvent.addGlobalMonitorForEvents(matching: .flagsChanged, handler: { (event) in
-               // update the caps lock status when a caps lock key event is received
+        // update the caps lock status when a caps lock key event is received
             self.capsLockStatus = event.modifierFlags.contains(.capsLock)
-           })
+        })
         // create the menu
         let menu = NSMenu()
 
@@ -43,5 +48,23 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
 
         // set the menu for the menu bar item
         menuBarItem.menu = menu
+        let src = CGEventSource(stateID: CGEventSourceStateID.hidSystemState)
+
+        let cmdd = CGEvent(keyboardEventSource: src, virtualKey: 0x38, keyDown: true)
+        let cmdu = CGEvent(keyboardEventSource: src, virtualKey: 0x38, keyDown: false)
+
+        let loc = CGEventTapLocation.cghidEventTap
+
+        cmdd?.post(tap: loc)
+        cmdu?.post(tap: loc)
+        
+    }
+    private func updateMenuBarIcon() {
+        // update the icon to reflect the caps lock status
+        if capsLockStatus {
+            menuBarItem.button?.image = NSImage(named: "caps-lock-on-24")
+        } else {
+            menuBarItem.button?.image = NSImage(named: "caps-lock-off-24")
+        }
     }
 }
